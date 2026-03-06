2210130e316dfe27cbd902770284139e6aed5e12
realsebyake <149763501+realsebyake@users.noreply.github.com>
2026-03-06 20:57:04 +0200

    extvar exercises 1-2

--- Chapter 1/ExtVarFinalExercises.c
+++ Chapter 1/ExtVarFinalExercises.c
@@ -1,8 +1,31 @@
+// 1.10 Exercises
 #include <stdio.h>
+
+#define TABSIZE 8
 
 int main() {
 
+    int col, spaces, c, i;
+    col = 0;
+    spaces = 0;
 
-
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            putchar(c);
+            col = 0;
+        }
+        else if (c == '\t') {
+            spaces = TABSIZE - (col % TABSIZE);
+            for (i = 0; i < spaces; ++i) {
+                putchar(' ');
+                ++col;
+            }
+        }
+        else {
+        putchar(c);
+        ++col;
+        }
+    }
 return 0;
 }
+
