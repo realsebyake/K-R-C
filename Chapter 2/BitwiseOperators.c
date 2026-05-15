b56484d6af6c753309dc70e97c7ab48f7ef549ce
realsebyake <149763501+realsebyake@users.noreply.github.com>
2026-05-15 23:53:38 +0300

    finished bitwise

--- Chapter 2/BitwiseOperators.c
+++ Chapter 2/BitwiseOperators.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-int setbits(int x, int p, int n, int y) {
+unsigned setbits(unsigned x, int p, int n, int y) {
 
 return (x & ~(~(~0U << n) << (p + 1 - n))) | ((y &  ~(~0U << n)) << (p + 1 -n)); 
 
 }   
 
-int getbits(int x, int p, int n) {
+unsigned getbits(unsigned x, int p, int n) {
 
-return ((x & n) >> p);
+return  (~(~0U << n) & (x >> (p + 1 - n)));
     
+}
+
+unsigned invert(unsigned x, int p, int n) {
+
+return ((~(~0U << n) << p) ^ x);
+
+}
+
+unsigned rightrot(unsigned x, int n) {
+
+return (x & ~(~0U << n)) << (sizeof(unsigned) * 8 - n) | (x >> n);
+
 }
 
 int main() {
@@ -17,11 +29,13 @@ int main() {
 int x = 14;  // 00001110
 int y = 7;   // 00000111
 int p = 4;   // 00000010
-int n = 3;   // 00000011
+int n = 2;   // 00000010
 
 
-printf("%d\n", setbits(x, p, n, y));
-printf("%d\n", getbits(x, p, n));
+printf("%u\n", setbits(x, p, n, y));
+printf("%u\n", getbits(x, p, n));
+printf("%u\n", invert(x, p, n));
+printf("%u\n", rightrot(x, n));
 return 0;
 }
 
