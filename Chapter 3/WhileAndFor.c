9d689ff46a851cef998e8058cf6c9a6852da3841
realsebyake <149763501+realsebyake@users.noreply.github.com>
2026-05-21 22:00:26 +0300

    Finished Ex 3-3

---
+++ Chapter 3/WhileAndFor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void expand(char s1[], char s2[]);
+
+int main() {
+
+      char s2[100];
+
+      expand("a-z", s2);     printf("%s\n", s2);
+      expand("a-b-c", s2);   printf("%s\n", s2);
+      expand("a-z0-9", s2);  printf("%s\n", s2);
+      expand("-a-z", s2);    printf("%s\n", s2);
+      expand("a-z-", s2);    printf("%s\n", s2);
+
+      return 0;
+
+}
+
+void expand(char s1[], char s2[]) {
+
+    int i, j, k;
+    i = j = k = 0;
+
+    for (i = 0; s1[i] != '\0'; i++) {
+        if (s1[i] == '-') {
+            if ( i > 0 && (isalpha(s1[i-1]) || isdigit(s1[i-1])) && (isalpha(s1[i+1]) || isdigit(s1[i+1]))) {
+                for (k = s1[i-1] + 1; k <= s1[i+1]; k++) {
+                    s2[j] = k;
+                    j++;
+                }
+                i++;
+            }
+        }
+        else {
+            s2[j] = s1[i];
+            j++;
+        }
+    }
+    s2[j] = '\0';
+}
